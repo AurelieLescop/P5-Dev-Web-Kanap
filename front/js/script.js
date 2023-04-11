5e072f42fcbf6e2fcc6911198e6086d8ce32a781
Aurelie Lescop <lescop.aurelie@live.fr>
2023-04-11 17:30:52 +0200

    ajout de commentaires à script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,18 @@
-//* definitions des constantes*//
+/* definitions des constantes*/
  const urlInventory = "http://localhost:3000/api/products"
  const vignette = document.getElementById("items")
 
- //* fonction pour récupérer les articles*//
+ /* fonction pour récupérer les articles*/
  const getArticles = () => {
+    /*pour requêter l'API*/
         fetch(urlInventory)
+
+        /*récupérer la réponse émise*/
         .then(function (answer) {
             return answer.json()
         })
+
+        /*parcourir la fonction pour insérer chaque produit dans la page d'accueil*/
         .then(function (data) {
             for(product in data) {
                 vignette.innerHTML += `<a href="./product.html?id=42">

45b3ab38770acb1d83551c743fee6be7070cdebb
Aurelie Lescop <lescop.aurelie@live.fr>
2023-04-17 21:14:08 +0200

    reprise fichier script suite explications

--- front/js/script.js
+++ front/js/script.js
@@ -1,68 +1,109 @@
-/* definitions des constantes*/
- const urlInventory = "http://localhost:3000/api/products"
- /*const vignette = document.getElementById("items")*/
 
- /* fonction pour récupérer les articles*/
- const getArticles = () => {
-    /*pour requêter l'API*/
-        fetch(urlInventory)
+const urlInventory = "http://localhost:3000/api/products"
 
-        /*récupérer la réponse émise*/
-        .then(function (answer) {
-            return answer.json()
-        })
+async function getArticles () {
 
-        /*parcourir la fonction pour insérer chaque produit dans la page d'accueil*/
-        .then(function (data) {
-            console.log(data)
-            /*for(product in data) {*/
+  const res = await fetch(urlInventory)
+  const data = await res.json()
 
-            // data.forEach(function (product) {
-            // data.forEach(product => {
-              data.forEach((product) => {
-                
-                console.log(product)
+  console.log(data)
 
-                
-                /*
-                vignette.innerHTML += `<a href="./product.html?id=42">
-                document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
-                <article>
-                  <img src="${product.imageUrl}" alt="${product.altTxt}">
-                  <h3 class="productName">${product.name}</h3>
-                  <p class="productDescription">${product.description}</p>
-                </article>
-              </a>`*/
-            })
-        })
+ for (let canape of data) {
+  const container = document.getElementById("items")
+  const a = document.createElement ("a")
+  container.appendChild(a)
+  const article = document.createElement ("article")
+  a.appendChild(article)
+  const img = document.createElement ("img")
+  article.appendChild(img)
+  img.setAttribute ("src", canape.imageUrl)
+  console.log (canape)
  }
 
- async function getArticles2() {
-  /*pour requêter l'API*/
-      const answer = await fetch(urlInventory);
-      const data = await answer.json();
+ /*
+          <a href="./product.html?id=42">
+            <article>
+              <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">Kanap name1</h3>
+              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a>
+ */
 
-      // console.log(data)
 
-      for (id in data) {
-        console.log(`ID: ${id}`);
-      }
-      for (product of data) {
-      // data.forEach(product => {
-          
-          console.log(`Product: ${product.name}`);
-
-          
-          /*
-          vignette.innerHTML += `<a href="./product.html?id=42">
-          document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
-          <article>
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            <h3 class="productName">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
-          </article>
-        </a>`*/
-      }
+ 
+//  for (let i=0; i<data.length; i=i+1) {
+//console.log(data[i])
+//}
 }
 
- getArticles2()
+getArticles()
+
+// /* definitions des constantes*/
+//  const urlInventory = "http://localhost:3000/api/products"
+//  /*const vignette = document.getElementById("items")*/
+
+//  /* fonction pour récupérer les articles*/
+//  const getArticles = function () {
+//     /*pour requêter l'API*/
+//         fetch(urlInventory)
+
+//         /*récupérer la réponse émise*/
+//         .then(function (answer) {
+//             return answer.json()
+//         })
+
+//         /*parcourir la fonction pour insérer chaque produit dans la page d'accueil*/
+//         .then(function (data) {
+//             console.log(data)
+//             /*for(product in data) {*/
+
+//             // data.forEach(function (product) {
+//             // data.forEach(product => {
+//               data.forEach((product) => {
+                
+//                 console.log(product)
+
+                
+//                 /*
+//                 vignette.innerHTML += `<a href="./product.html?id=42">
+//                 document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
+//                 <article>
+//                   <img src="${product.imageUrl}" alt="${product.altTxt}">
+//                   <h3 class="productName">${product.name}</h3>
+//                   <p class="productDescription">${product.description}</p>
+//                 </article>
+//               </a>`*/
+//             })
+//         })
+//  }
+
+//  async function getArticles2() {
+//   /*pour requêter l'API*/
+//       const answer = await fetch(urlInventory);
+//       const data = await answer.json();
+
+//       // console.log(data)
+
+//       for (id in data) {
+//         console.log(`ID: ${id}`);
+//       }
+//       for (product of data) {
+//       // data.forEach(product => {
+          
+//           console.log(`Product: ${product.name}`);
+
+          
+//           /*
+//           vignette.innerHTML += `<a href="./product.html?id=42">
+//           document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
+//           <article>
+//             <img src="${product.imageUrl}" alt="${product.altTxt}">
+//             <h3 class="productName">${product.name}</h3>
+//             <p class="productDescription">${product.description}</p>
+//           </article>
+//         </a>`*/
+//       }
+// }
+
+//  getArticles2()
--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-/* definitions des constantes*/
+///* definitions des constantes*/
 const urlInventory = "http://localhost:3000/api/products"
 /*const vignette = document.getElementById("items")*/
 
@@ -108,3 +108,77 @@ async function main() {
 (async function() {
     await main();
 })();
+
+
+--------------------------
+
+//collage version actuelle de script.js au 16/04
+
+/* definitions des constantes*/
+const urlInventory = "http://localhost:3000/api/products"
+/*const vignette = document.getElementById("items")*/
+
+/* fonction pour récupérer les articles*/
+const getArticles = () => {
+   /*pour requêter l'API*/
+       fetch(urlInventory)
+
+       /*récupérer la réponse émise*/
+       .then(function (answer) {
+           return answer.json()
+       })
+
+       /*parcourir la fonction pour insérer chaque produit dans la page d'accueil*/
+       .then(function (data) {
+           console.log(data)
+           /*for(product in data) {*/
+
+           // data.forEach(function (product) {
+           // data.forEach(product => {
+             data.forEach((product) => {
+               
+               console.log(product)
+
+               
+               /*
+               vignette.innerHTML += `<a href="./product.html?id=42">
+               document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
+               <article>
+                 <img src="${product.imageUrl}" alt="${product.altTxt}">
+                 <h3 class="productName">${product.name}</h3>
+                 <p class="productDescription">${product.description}</p>
+               </article>
+             </a>`*/
+           })
+       })
+}
+
+async function getArticles2() {
+ /*pour requêter l'API*/
+     const answer = await fetch(urlInventory);
+     const data = await answer.json();
+
+     // console.log(data)
+
+     for (id in data) {
+       console.log(`ID: ${id}`);
+     }
+     for (product of data) {
+     // data.forEach(product => {
+         
+         console.log(`Product: ${product.name}`);
+
+         
+         /*
+         vignette.innerHTML += `<a href="./product.html?id=42">
+         document.getElementById("items").innerHTML += `<a href="./product.html?id=42">
+         <article>
+           <img src="${product.imageUrl}" alt="${product.altTxt}">
+           <h3 class="productName">${product.name}</h3>
+           <p class="productDescription">${product.description}</p>
+         </article>
+       </a>`*/
+     }
+}
+
+getArticles2()

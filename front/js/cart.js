989d4411b09dbc8d12b0dc1b2379e24007dcf4de
Aurelie Lescop <lescop.aurelie@live.fr>
2023-04-26 18:03:42 +0200

    recuperation arraylocalstorage ds page cart.js

---
+++ front/js/cart.js
@@ -0,0 +1,8 @@
+
+let productsLoadedInLocalStorage = JSON.parse(localStorage.getItem("cartProduct"));
+console.log(productsLoadedInLocalStorage);
+
+console.log("Hello")
+console.log(JSON.parse(localStorage.getItem("cartProduct")))
+console.log(productsLoadedInLocalStorage[0].quantity)
+

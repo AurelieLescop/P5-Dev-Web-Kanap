fa31a1f7c08dff51b5a9f6cda020a097b7e1664b
Aurelie Lescop <lescop.aurelie@live.fr>
2023-04-29 14:29:55 +0200

    intégration données panier cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,43 +7,6 @@ console.log(JSON.parse(localStorage.getItem("cartProduct")))
 console.log(productsLoadedInLocalStorage[0].quantity)
 console.log(productsLoadedInLocalStorage[0].price)
 
-/*
-for (let canape of data) {
-
-  //endroit intégration dans index.html
-  const container = document.getElementById("items")
-  
-  // creation d'un element
-  const newElement = createCanapeElement (canape)
-  
-  // rattachement de l'element et intégration à index.html
-  container.appendChild(newElement)
- }
-
-function createCanapeElement (canape) {
-  const a = createElementA (canape)
-  const article = createElementArticle (a)
-  createElementImg (article, canape)
-  createElementH3 (canape, article)
-  createElementp (canape, article)
-
-  console.log (canape)
-
-  return a
-}
-
-function createElementA (canape) {
-    const a = document.createElement ("a")
-    a.setAttribute ("href", "./product.html?id="+canape._id)
-    return a
-  }
-  
-    //creation article et rattachement à index.html
-function createElementArticle (a) {
-    const article = document.createElement ("article")
-    a.appendChild(article)
-    return article
-  }*/
 
     /*
     - créer article et attribuet class, data-id et data-colors
@@ -74,119 +37,112 @@ function createElementArticle (a) {
     article.setAttribute("data-id", product.productId)
     article.setAttribute("data-color", product.color)
 
+    //creation div class = cart__item__img
+    const divImg = document.createElement ("div")
+    article.appendChild(divImg)
+    divImg.setAttribute("class", "cart__item__img")
+
     // creation Img
     const img = document.createElement ("img")
-    article.appendChild(img)
+    divImg.appendChild(img)
     img.setAttribute ("src", product.imageUrl)
-    img.setAttribute ("alt", product.altTxt)
-
+    img.setAttribute ("alt", product.altText)
+    console.log(product)  
     
+     //creation div class = cart__item__content
+    const divContent = document.createElement ("div")
+    article.appendChild(divContent)
+    divContent.setAttribute("class", "cart__item__content")
+
+     //creation div class = cart__item__content__description
+    const divContentDescription = document.createElement ("div")
+    divContent.appendChild(divContentDescription)
+    divContentDescription.setAttribute("class", "cart__item__content")
+
+     //creation titre h2
+     const h2 = document.createElement ("h2")
+     const h2Content = document.createTextNode(product.name)
+     h2.appendChild(h2Content)
+     divContentDescription.appendChild(h2)
+
+     //creation paragraphe p couleur
+     const pColor = document.createElement ("p")
+     const pColorContent = document.createTextNode(product.color)
+     pColor.appendChild(pColorContent)
+     divContentDescription.appendChild(pColor)
+
+    //creation paragraphe p prix
+     const pPrice = document.createElement ("p")
+     const pPriceContent = document.createTextNode(product.price + " €") 
+     pPrice.appendChild(pPriceContent)
+     divContentDescription.appendChild(pPrice)
+
+    //creation div class = cart__item__content__settings
+    const divContentSettings = document.createElement ("div")
+    divContent.appendChild(divContentSettings)
+    divContentSettings.setAttribute("class", "cart__item__content__settings")
+
+    //creation div class = cart__item__content__settings__quantity
+    const divContentSettingsQuantity = document.createElement ("div")
+    divContentSettings.appendChild(divContentSettingsQuantity)
+    divContentSettingsQuantity.setAttribute("class", "cart__item__content__settings__quantity")
+
+    //creation paragraphe p prix
+    const pQuantity = document.createElement ("p")
+    const pQuantityContent = document.createTextNode("Qté : ")
+    pQuantity.appendChild(pQuantityContent)
+    divContentSettingsQuantity.appendChild(pQuantity)
+
+    //creation tab input quantity
+    const inputQuantity = document.createElement ("input")
+    divContentSettingsQuantity.appendChild(inputQuantity)
+    inputQuantity.setAttribute("type", "number")
+    inputQuantity.setAttribute("class", "itemQuantity")
+    inputQuantity.setAttribute("name", "itemQuantity")
+    inputQuantity.setAttribute("min", "1")
+    inputQuantity.setAttribute("max", "100")
+    inputQuantity.setAttribute("value", product.quantity)
+
+    //creation div class = cart__item__content__settings__delete
+    const divContentSettingsDelete = document.createElement ("div")
+    divContentSettings.appendChild(divContentSettingsDelete)
+    divContentSettingsDelete.setAttribute("class", "cart__item__content__settings__delete")
+
+
+    //creation p class = cart__item__content__settings__delete
+    const pDelete = document.createElement ("p")
+    const pDeleteContent = document.createTextNode("Supprimer")
+    pDelete.appendChild(pDeleteContent)
+    divContentSettingsDelete.appendChild(pDelete)
+    pDelete.setAttribute("class", "deleteItem")
+
     // rattachement de l'element et intégration à index.html
     }
-    console.log(product)
-    return product
-    
+  
     }
     
     createCart()
 
+   // function removeFromCart(product)
 
-
-/*function createCart(productsLoadedInLocalStorage) {
-for (let product of productsLoadedInLocalStorage) {
 /*
-
-
-// endroit intgration dans html
-const container = document.getElementById("cart__items")
-
-// creation d'un element
-//const newElement = createCartElement (product)
-
-const a = document.createElement ("a")
-a.setAttribute ("href", "./product.html?id="+ product.id)
-
-const article = document.createElement ("article")
-a.appendChild(article)
-
-
-
-// rattachement de l'element et intégration à index.html
-container.appendChild(article)
-}
-}
-
-createCart(productsLoadedInLocalStorage)*/
-
-//function createCartElement (a) {
-
-  //}
-
-
-
-
-// Creation article
-
-
-
-/* <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-            <div class="cart__item__img">
-                <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>Nom du produit</h2>
-                    <p>Vert</p>
-                    <p>42,00 €</p>
-                </div>
-            <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                    <p>Qté : </p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem">Supprimer</p>
-                </div>
-            </div>
-        </div>
-</article>*/
-
-//Creation article
-
-
-/*function createImg(data) {
-  const img = document.createElement("img")
-  img.setAttribute("src", data.imageUrl)
-  img.setAttribute("alt", data.altTxt)
-  return img
-}
-
-function integrateImg(img) {
-  const container = document.querySelector('.item__img')
-  container.appendChild(img)
-  return img
-}
-
-//ajout du titre h1 lié au canapé spécifique ds la page product.html
-function addTitleContentH1(data) {
-  document.querySelector('h1').textContent = data.name;
-  console.log(data.name);
-}
-
-//ajout du prix dans id=price lié au canapé spécifique ds la page product.html
-function addContentPrice(data) {
-  document.querySelector('#price').textContent = data.price
-}
-
-//ajout de la description dans id=description lié au canapé spécifique ds la page product.html
-function addContentDescription(data) {
-  document.querySelector('#description').textContent = data.description
-}
-
 function createElement(data) {
   const img = createImg(data)
   integrateImg(img)
   addTitleContentH1(data)
   addContentPrice(data)
   addContentDescription(data)
+}*/
+
+/*
+function createCanapeElement (canape) {
+  const a = createElementA (canape)
+  const article = createElementArticle (a)
+  createElementImg (article, canape)
+  createElementH3 (canape, article)
+  createElementp (canape, article)
+
+  console.log (canape)
+
+  return a
 }*/
